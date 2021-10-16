eecfead6485e022062d6fdda38fee973241595f9
suthirakprom <suthirakprom81@gmail.com>
2021-10-16 09:33:00 +0700

    first commit

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,32 @@
+import classes from "./MealItem.module.css";
+import { useContext } from "react";
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+
+const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const price = `$${props.price.toFixed(2)}`;
+  const addItemToCartHandler = (amount) => {
+    cartCtx.addItem({
+      amount: amount,
+      name: props.name,
+      id: props.id,
+      price: props.price,
+    });
+  };
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm onAddToCart={addItemToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,34 @@
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import { Fragment } from "react";
+
+const Backdrop = (props) => {
+  return <div onClick={props.onCloseCart} className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseCart={props.onCloseCart} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
